c9eca1983bca720a44dd971a3a86d71abf323349
Owie <151057755+Owie6789@users.noreply.github.com>
2025-06-11 18:10:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -45,12 +45,6 @@ const statusMessages = [
 
 let currentIndex = 0;
 
-function updateStatus() {
-  const currentStatus = statusMessages[currentIndex];
-  client.user.setPresence({
-    activities: [{ name: currentStatus.name, type: currentStatus.type }],
-  });
-
   console.log('\x1b[33m[ STATUS ]\x1b[0m', `Updated status to: ${currentStatus.name} (${ActivityType[currentStatus.type]})`);
   currentIndex = (currentIndex + 1) % statusMessages.length;
 }
